eaf61ab325a89b46f3f374f92f40d8bcc16489ba
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-14 00:36:36 +0200

    Create Two Sum.py
    https://leetcode.com/problems/two-sum/submissions/1174586777/

---
+++ Two Sum.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict1 = defaultdict()
+        for i in range(len(nums)):
+            dict1[nums[i]] = i
+        for i in range(len(nums)):
+            if target-nums[i] in dict1 and i!=dict1[target-nums[i]]:
+                return [i,dict1[target-nums[i]]]
+ 
+        
