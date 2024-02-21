b94e1fc4feed1f2407e4bdcee3e419798e015301
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-21 23:52:29 +0200

    Update 1823. Find the Winner of the Circular Game.py
    https://leetcode.com/problems/find-the-winner-of-the-circular-game/submissions/1182417408/

--- 1823. Find the Winner of the Circular Game.py
+++ 1823. Find the Winner of the Circular Game.py
@@ -3,8 +3,8 @@ class Solution:
         l1 = [i for i in range(1, n + 1)]
         removed = 0
         while len(l1) > 1:
-            print(l1)
-            print(removed)
+            #print(l1)
+            #print(removed)
             removed = (removed+ k - 1) % len(l1)
             l1.remove(l1[removed])
 
