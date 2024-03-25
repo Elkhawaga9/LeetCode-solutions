2c50975b74b0bacfb41240b8be93c98c07c34fbd
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-03-25 14:08:05 +0200

    Time: 716 ms (24.94%), Space: 24 MB (53.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -4,7 +4,7 @@ class Solution:
     def totalFruit(self, fruits: list[int]) -> int:
         unique = defaultdict(int)
         left = 0
-        ans = 0
+        ret = 0
         for right in range(len(fruits)):
             unique[fruits[right]]+=1
             while(len(unique)>2):
@@ -12,5 +12,5 @@ class Solution:
                 if unique[fruits[left]]==0:
                     del unique[fruits[left]]
                 left += 1
-            ans = max(ans,right-left+1)
-        return ans
+            ret = max(ret,right-left+1)
+        return ret
