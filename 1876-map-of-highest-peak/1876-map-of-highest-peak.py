585f9f927b83cb092a8ce057014260665c04270d
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-07-02 22:43:37 +0300

    Time: 3357 ms (22.82%), Space: 214.2 MB (16.3%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.py
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.py
@@ -2,6 +2,7 @@ class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         n,m = len(isWater),len(isWater[0])
         has = deque()
+        visited = set()
         for i in range(n):
             for j in range(m):
                 if isWater[i][j]==1:
@@ -11,15 +12,17 @@ class Solution:
                     isWater[i][j] = -1
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         def valid(x,y):
-            return 0<=x<n and 0<=y<m
+            return 0 <= x < n and 0 <= y < m
         while has:
             i,j = has.popleft()
             for dx,dy in directions:
                 x = i+dx
                 y = j+dy
-                if valid(x,y) and isWater[x][y]==-1:
+                if valid(x,y) and isWater[x][y]!=0 and (x,y) not in visited:
                     isWater[x][y] = isWater[i][j] + 1
-                    has.append([x, y])
+                    has.append([x,y])
+                    visited.add((x,y))
+                 
         return isWater
 
 
