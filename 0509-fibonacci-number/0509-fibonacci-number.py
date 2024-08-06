08b11ae2022a9c1c9d76092e8389ff0df833307e
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-08-06 20:57:40 +0300

    Time: 27 ms (95.65%), Space: 16.5 MB (51.77%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,11 @@
+sav = dict()
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<2:
+            return n
+        if n in sav:
+            return sav[n]
+        else:
+            sav[n] = self.fib(n-1)+self.fib(n-2)
+            return sav[n]
