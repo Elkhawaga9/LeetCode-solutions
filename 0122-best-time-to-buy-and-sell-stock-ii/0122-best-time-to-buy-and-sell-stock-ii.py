6a90852f63a6e21ecfbd6350a3ea3b2dce9a9994
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-08-24 15:09:28 +0300

    Time: 98 ms (5.36%), Space: 26.4 MB (5.01%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        memo = {}
+        def help(idx,holding):
+            if idx==n:
+                return 0
+            if (idx,holding) in memo:
+                return memo[(idx,holding)]
+            ans1 = 0
+            ans2 = 0
+            if holding:
+                Sell = help(idx+1,False) + prices[idx]
+                Leave = help(idx+1,True)
+                ans1 = max(Sell,Leave)
+                memo[(idx,holding)] = ans1
+            else:
+                Buy = help(idx+1,True) - prices[idx]
+                Leave = help(idx+1,False)
+                ans2 = max(Buy,Leave)
+                memo[(idx,holding)] = ans2
+            return max(ans1,ans2)
+            
+        return help(0,False)
+            
+        
