8bc100185ea1f3a06d58662b855bfb115c0ba699
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-08-26 12:37:15 +0300

    Time: 136 ms (93.38%), Space: 16.3 MB (99.58%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,7 +2,7 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
         ans = 0
-        memo = {}
+
         def help(l,r):
             cnt = 0
             while l>=0 and r<n and s[l]==s[r]:
@@ -10,7 +10,7 @@ class Solution:
                 l-=1
                 r+=1
             return cnt 
-            
+
         for i in range(n):
             ans += help(i,i)
             ans += help(i,i+1)
